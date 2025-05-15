fc9fc3e391fae3090d124c90ed129451cedad795
Fabián Abarca <ensinergia@gmail.com>
2025-05-15 16:42:14 -0300

    Eliminar lo relacionado con las plantillas de company, schedule y realtime

--- views.py
+++ views.py
@@ -5,15 +5,3 @@ from django.shortcuts import render
 
 def gtfs(request):
     return render(request, "gtfs.html")
-
-
-def schedule(request):
-    return render(request, "schedule.html")
-
-
-def realtime(request):
-    return render(request, "realtime.html")
-
-
-def company(request):
-    return render(request, "company.html")
